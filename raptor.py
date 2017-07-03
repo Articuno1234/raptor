f5e18e42c9dfc28c2d5b7b27e28815ad334b73f1
Articuno1234 <Articuno1234@users.noreply.github.com>
2017-07-03 17:44:49 +1000

    Update raptor.py

--- raptor.py
+++ raptor.py
@@ -1,1 +1,21 @@
+try:
+  import sys
+  import os
+  import subprocess
+  import random
+except ImportError:
+  input("Raptor can not be used\n"
+        "Some requirements are not installed")
+  
+def exit(num):
+  sys.exit(num)
+  
+def dice():
+  dice = ["1", "2", "3", "4", "5", "6"]
+  random.choice(dice)
 
+def print(args):
+  print(args)
+  
+def pip(args):
+  subprocess.call(("pip3", "install", args)
